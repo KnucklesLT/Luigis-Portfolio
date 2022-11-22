8a0093ccfea9efe6516aae1182446f6c1a8c182c
Luigi T <luigitejada@yahoo.com>
2022-11-22 11:18:49 -0500

    edit about page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         </li>
         <li>
           <NavLink to='about'>
-            About
+            About Me
           </NavLink>
         </li>
         <li>
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,12 @@
+import myPhoto from '../../assets/branding/me.jpg'
+import styles from './About.module.css'
+
 const About = () => {
   return ( 
-    <h1>About</h1>
+    <>
+      <h1>About</h1>
+      <img className={styles.image} src={myPhoto} alt="Luigi" />
+    </>
   );
 }
 
